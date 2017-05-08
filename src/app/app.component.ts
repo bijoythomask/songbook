2145940b54bd06265cb35becefce1b0e91bc06e8
bijoythomask <bijoythomask@yahoo.com>
2017-05-08 19:01:22 +0530

    first material example

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
-import {MdButtonModule, MdCheckboxModule} from '@angular/material';
+import { MdButtonModule , MdCheckboxModule } from '@angular/material';
+import { MdInputModule } from '@angular/material';
 
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
   title = 'app works!';
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,7 +2,8 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
-import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MdInputModule } from '@angular/material';
 
 import 'hammerjs';
 
@@ -16,7 +17,8 @@ import { AppComponent } from './app.component';
     BrowserModule,
     FormsModule,
     HttpModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    MdInputModule
   ],
   providers: [],
   bootstrap: [AppComponent]
